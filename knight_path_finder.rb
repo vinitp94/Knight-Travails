f73697eae8da97040646c060869d79a7fc25ae01
vinit_parikh <vinitp94@gmail.com>
2016-11-18 14:36:50 -0800

    Finish KnightPathFinder

--- knight_path_finder.rb
+++ knight_path_finder.rb
@@ -1,7 +1,7 @@
 require_relative 'poly_tree_node'
 
 class KnightPathFinder
-  attr_reader :node
+  attr_reader :root
 
   BOARD_SIZE = 8
   DELTA = [
@@ -15,9 +15,9 @@ class KnightPathFinder
     [-2, 1]
   ].freeze
 
-
   def initialize(pos = [0, 0])
-    @node = PolyTreeNode.new(pos)
+    @root = PolyTreeNode.new(pos)
+    build_move_tree
   end
 
   def new_move_positions(pos)
@@ -32,11 +32,33 @@ class KnightPathFinder
   end
 
   def build_move_tree
-
+    queue = [root]
+    until queue.empty?
+      current = queue.shift
+      new_move_positions(current.value).each do |move|
+        unless root.dfs(move)
+          child = PolyTreeNode.new(move)
+          child.parent = current
+          queue.push(child)
+        end
+      end
+    end
   end
 
   def find_path(pos)
-
+    path = []
+    target = root.dfs(pos)
+    until target.parent.nil?
+      path.unshift(target.value)
+      target = target.parent
+    end
+    path.unshift(root.value)
+    path
   end
 
 end
+
+kpf = KnightPathFinder.new
+
+p kpf.find_path([7, 6]) # => [[0, 0], [1, 2], [2, 4], [3, 6], [5, 5], [7, 6]]
+p kpf.find_path([6, 2]) # => [[0, 0], [1, 2], [2, 0], [4, 1], [6, 2]]
